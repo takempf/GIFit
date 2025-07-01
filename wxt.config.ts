d21a57895eebecc1276d08744c9fc6b168b6ba1b
Timothy Kempf <tim@kempf.dev>
2025-06-30 19:49:40 -0700

    add storage values for config panel data that i want persisted

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   manifest: {
+    permissions: ['storage'],
     host_permissions: ['*://*.youtube.com/*']
   },
   srcDir: 'src',
