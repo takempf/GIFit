ace60b963b32e763e49643c31c95458685c84ae3
Timothy Kempf <tim@kempf.dev>
2026-02-15 12:07:05 -0800

    improve the extension's demo page

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+
 import { App as SharedApp } from '@gifit/shared/components/App';
 import { AdapterContext, AdapterSet } from '@gifit/shared/adapters/context';
 import {
@@ -9,6 +10,9 @@ import {
 import { videoController } from '@gifit/shared/services/VideoController';
 import { storageAdapter } from '@gifit/shared/utils/storage';
 
+import GifitLogo from '@gifit/shared/assets/gifit-logo.svg?react';
+import GifitIcon from '@gifit/shared/assets/gifit-icon.svg?react';
+
 import './App.css';
 
 // Mock video source (Big Buck Bunny Trailer)
@@ -16,6 +20,7 @@ const VIDEO_SRC = '/videos/big-buck-bunny-trailer.mp4';
 
 export default function App() {
   const [adapters, setAdapters] = useState<AdapterSet | null>(null);
+  const [isReady, setIsReady] = useState(false);
 
   useEffect(() => {
     const getVideo = () => document.querySelector('video');
@@ -32,43 +37,64 @@ export default function App() {
       video: videoAdapter,
       gif: gifAdapter,
       storage: storageAdapterImpl,
-      getVideoTitle: async () => 'Demo Video Title'
+      getVideoTitle: async () => 'Big Buck Bunny'
     });
   }, []);
 
   return (
-    <div className="container">
-      <h1 className="heading">GIFit! Web Demo</h1>
+    <main id="main">
+      <div className="logo">
+        <GifitLogo />
+      </div>
+      <section className="conversation">
+        <p>
+          Hi, I'm <a href="https://kempf.dev">Tim</a>!
+        </p>
+        <p>
+          With this extension you can create GIFs from any video on YouTube,
+          without ever leaving your browser.
+        </p>
+        <p>
+          If that sounds good to you, you can{' '}
+          <a href="https://chromewebstore.google.com/detail/gifit/khoojcphcmgcplkpckkjpdlloooifgec">
+            add GIFit to Chrome
+          </a>{' '}
+          right away!
+        </p>
+        <p>You can also try it out below with a demo video:</p>
+      </section>
 
-      <div className="videoWrapper">
+      <section className="demo">
         <video
           src={VIDEO_SRC}
           controls
           className="video"
           crossOrigin="anonymous"
+          onLoadedMetadata={(e) => {
+            e.currentTarget.currentTime = 4.5;
+          }}
+          onSeeked={() => setIsReady(true)}
         />
 
+        <div className="source">
+          Video Source:{' '}
+          <a href="https://www.youtube.com/watch?v=aqz-KE-bpKQ" target="_blank">
+            Big Buck Bunny
+          </a>{' '}
+          Trailer
+        </div>
+
         {/* Simulate the extension popup overlay */}
-        {adapters && (
+        {adapters && isReady && (
           <div className="popupOverlay">
-            <div className="popupFrame" style={{ width: 420 }}>
+            <div className="popupFrame">
               <AdapterContext.Provider value={adapters}>
                 <SharedApp />
               </AdapterContext.Provider>
             </div>
           </div>
         )}
-      </div>
-
-      <p>This is a &quot;web&quot; version of the GIFit! extension.</p>
-      <p>
-        It uses a &quot;virtual&quot; content script to simulate the extension
-        environment.
-      </p>
-      <p className="description">
-        This demo simulates the extension running on a YouTube video. The
-        &quot;popup&quot; is rendered as an overlay on the video player.
-      </p>
-    </div>
+      </section>
+    </main>
   );
 }
